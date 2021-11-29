9f3bd005ed11e74e3d9e3dfa104022e521d9d6e3
kenyon-bunker <92130048+kenyon-bunker@users.noreply.github.com>
2021-11-29 16:54:35 -0700

    Update household.sql

--- test_cases/household.sql
+++ test_cases/household.sql
@@ -1,1 +1,29 @@
 #Test case for Household
+USE CIT225Project;
+
+#Create
+INSERT INTO household (name)
+VALUES ('Bunkers');
+
+Insert into household (name)
+values ('Smiths');
+
+Insert into household (name)
+values ('Does');
+
+Insert into household (name)
+values ('asdad');
+
+#Read
+select * from household;
+
+#Update
+update household
+SET name = 'Johnsons'
+where name = 'asdad';
+
+#Delete
+delete from household
+where name = 'Johnsons';
+
+
